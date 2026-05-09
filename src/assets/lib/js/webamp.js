fe988cdc93f00eb44f50871bc5547dd2cbba8fd4
maya <puppymaya@T14.localdomain>
2026-05-08 22:44:16 -0700

    fix links + layout

--- src/assets/lib/js/webamp.js
+++ src/assets/lib/js/webamp.js
@@ -3,25 +3,11 @@
 // file, or served with permissive CORS HTTP headers:
 // https://docs.webamp.org/docs/guides/cors
 
-
-function show() {
-            /* Access image by id and change 
-            the display property to block*/
-            document.getElementById('webamp')
-                .style.display = "block";
-            document.getElementById('sprite')
-                .style.display = "none";
-}
-
-
 import Webamp from "https://unpkg.com/webamp@^2/butterchurn";
 
 const webamp = new Webamp({
   initialTracks: [
-    {
-      metaData: { artist: "madmadmaya", title: "quickie", },
-      url: "/src/assets/lib/audio/quickie.wav",
-    },
+    { metaData: { artist: "madmadmaya", title: "quickie", }, url: "/src/assets/lib/audio/quickie.wav", },
         
   ],
   initialSkin: { url: "/src/assets/lib/makemesad.wsz"},
